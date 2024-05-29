da78a921d94e3881798f1498adb8ba245258f206
javier-fernandez-imatia <javier.fernandez@imatia.com>
2024-05-29 14:30:33 +0200

    Scroll changes

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, Injector, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
-import { Expression, FilterExpressionUtils, OCheckboxComponent, OGridComponent, OSearchInputComponent } from 'ontimize-web-ngx';
+import { Expression, FilterExpressionUtils, OCheckboxComponent, OFormComponent, OGridComponent, OSearchInputComponent } from 'ontimize-web-ngx';
 import { DummyService } from '../../shared/services/dummy.service';
 import { MatSidenav } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
@@ -25,6 +25,7 @@ export class HomeComponent implements OnInit {
   service: DummyService;
   imageService: ImageService;
   columns: number;
+  showClear: boolean;
 
   constructor(
     protected injector: Injector,
@@ -33,6 +34,7 @@ export class HomeComponent implements OnInit {
     this.service = this.injector.get(DummyService);
     this.imageService = this.injector.get(ImageService);
     this.media = this.injector.get(MediaObserver);
+    this.showClear = false;
   }
 
   ngOnInit(): void {
@@ -107,8 +109,10 @@ export class HomeComponent implements OnInit {
     });
 
     if (filters.length > 0) {
+      this.showClear = true;
       return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_OR));
     } else {
+      this.showClear = false;
       return FilterExpressionUtils.buildExpressionIn("TYPE", Array.from({ length: 10 }, (_, i) => i + 1));
     }
   }
