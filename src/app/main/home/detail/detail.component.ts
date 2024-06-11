b63183543a0c0b759d7af7e03c30a357defa8345
javier-fernandez-imatia <164156437+javier-fernandez-imatia@users.noreply.github.com>
2024-06-11 10:03:37 +0200

    Detail screen styles applied (#10)
    * Home screen changes applied
    
    * Home page styles updated
    
    * Dark mode implelmented on home page
    
    * Search input added and styles applied
    
    * Some style changesW
    
    * Detail screen styles applied
    
    * Fixed implemented
    
    * Gallery not working
    
    * Home page styles ready and templates display logic at page load changed
    
    * Removed font weight property because it was not working
    
    * Created image service and changed some img declarations
    
    * Fixed search filter styles
    
    * Application colors added to custom theme
    
    * Changed img route on scss files
    
    * Changed grid class selector
    
    * Styles changed to Ontimize standard
    
    * Style changes applied
    
    * Changed clear button text color
    
    * Fixed scroll appearing in the middle of the screen
    
    * Scroll changes
    
    * Hide clear btn not working
    
    * Update src/app/main/main.component.html
    
    Co-authored-by: Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
    
    * Clear button toggle working, hover cards implemented and scroll applied to all page
    
    * Removed unused code and fixed margins
    
    * Fixed grid item styles
    
    * Fixed hover state
    
    * Changed clear-btn class binding
    
    * Gallery component not working
    
    * Detail screen ready
    
    * Github icon color not showing correctly
    
    * Changed text ellipsis on detail screen
    
    * Set ratio 4:3 in o-gallery
    
    * Fixed icon registry
    
    * Removed unnecesary imports
    
    * Fixed Github button
    
    * Fixed console error and button styles
    
    * Changed start with flex-start
    
    * Merge commit
    
    * Merge commit
    
    * Changed detail height calc and flex properties displayed now on the template
    
    * Changed click event to onClick
    
    * Overflow now displayed on all the page
    
    * Changed detail scroll to right side only
    
    ---------
    
    Co-authored-by: Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
    Co-authored-by: IMATIASL\patricia.martinez <patricia.martinez@imatia.com>

--- src/app/main/home/detail/detail.component.ts
+++ src/app/main/home/detail/detail.component.ts
@@ -1,53 +1,97 @@
-import { Component, Injector, OnInit } from '@angular/core';
-import { DummyService } from '../../../shared/services/dummy.service';
+import { Component, Injector, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
+import { AppearanceService } from 'ontimize-web-ngx';
+import { GalleryImage } from 'ontimize-web-ngx-gallery';
+
+import { DummyService } from '../../../shared/services/dummy.service';
+import { ImageService } from '../../../shared/services/image.service';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.scss']
+  styleUrls: ['./detail.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DetailComponent implements OnInit {
   protected templateId: string;
   protected templateImg: string;
   protected templateTitle: string;
   protected templateDescription: string;
+  protected templateImages = [];
+  protected templateBottomDescription: string;
   private service: DummyService;
+  private imageService: ImageService;
+  protected galleryOptions = [
+    {
+      width: '100%',
+      thumbnailsColumns: 3,
+      aspectRatio: "5:3"
+    }
+  ];
+  protected galleryImages: GalleryImage[];
+  protected dark;
+  protected detail;
 
   constructor(
     protected injector: Injector,
-    protected router: Router
+    protected router: Router,
+    protected appeareanceService: AppearanceService
   ) {
     this.templateId = window.location.href.split('/')[5];
     this.templateId = this.templateId.split('?')[0];
     this.service = this.injector.get(DummyService);
+    this.imageService = this.injector.get(ImageService);
+    this.galleryImages = [];
   }
+
 
   ngOnInit(): void {
     this.configureService();
     this.queryTemplate();
-  }
-
-  protected configureService() {
-    const conf = this.service.getDefaultServiceConfiguration('templates');
-    this.service.configureService(conf);
-  }
-
-  queryTemplate() {
-    const filter = {
-      "ID": +this.templateId
-    };
-    const columns = ["IMG","TITLE", "DESCRIPTION", "TYPE"];
-    this.service.query(filter, columns, 'template').subscribe((response) => {
-      if (response.code === 0) {
-        this.templateImg = response.data[0].IMG;
-        this.templateTitle  = response.data[0].TITLE;
-        this.templateDescription = response.data[0].DESCRIPTION;
+    this.appeareanceService.isDarkMode$.subscribe(dark => {
+      if (dark) {
+        this.dark = "-dark";
+      } else {
+        this.dark = "";
       }
     });
   }
 
-  openHome() {
+  private loadGallery(templateImages) {
+    this.galleryImages = [];
+    templateImages.forEach(img => {
+      this.galleryImages.push({
+        "small": this.imageService.getImgUrl(img),
+        "medium": this.imageService.getImgUrl(img), "big": this.imageService.getImgUrl(img)
+      });
+    });
+  }
+
+  private configureService() {
+    const conf = this.service.getDefaultServiceConfiguration('templates');
+    this.service.configureService(conf);
+  }
+
+  private queryTemplate() {
+    const filter = {
+      "ID": +this.templateId
+    };
+    const columns = ["IMG", "TITLE", "DESCRIPTION", "TYPE", "IMAGES", "BOTTOM-DESCRIPTION"];
+    this.service.query(filter, columns, 'template').subscribe((response) => {
+      if (response.code === 0 && Array.isArray(response.data)) {
+        this.templateImg = response.data[0].IMG;
+        this.templateTitle = response.data[0].TITLE;
+        this.templateDescription = response.data[0].DESCRIPTION;
+        this.loadGallery(response.data[0].IMAGES);
+        if (response.data[0].BOTTOM_DESCRIPTION != null) {
+          this.templateBottomDescription = response.data[0].BOTTOM_DESCRIPTION;
+          this.detail = true;
+        }
+      }
+    });
+  }
+
+  public openHome() {
     this.router.navigate(['main/templates']);
   }
 }
