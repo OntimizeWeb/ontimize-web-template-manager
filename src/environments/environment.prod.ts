59f2bf6080d4738a2a632a7782065e1e9a19c22e
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2024-06-24 12:21:51 +0200

    Updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  githubUrl: 'https://github.com/OntimizeWeb/ontimize-web-templates/blob/main/templates/'
+  githubUrl: 'https://github.com/OntimizeWeb/ontimize-web-templates/blob/main/templates/',
+  urlReadme:'/ontimize-web-templates/main/templates/'
 };
