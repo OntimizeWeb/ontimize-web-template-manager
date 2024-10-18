26afd2beab017de1d4e939c798cfd93ff2b2e74d
Javier Fernández Méndez <javier.fernandez@imatia.com>
2024-10-18 10:09:26 +0200

    Fixed detail navigation

--- src/app/shared/services/dummy.service.ts
+++ src/app/shared/services/dummy.service.ts
@@ -161,17 +161,19 @@ export class DummyService extends OntimizeEEService {
 
     const result = [];
     resp.data.forEach(element => {
-      if (filters.includes(element.ID) && kv["ID"]) {
-        result.push(element);
-      }
-      else if (filters.includes(element.TYPE) && kv["@basic_expression"]["lop"] == "TYPE" || filters.includes(element.TYPE) && kv["@basic_expression"]["rop"]["lop"] == "TYPE") {
-        result.push(element);
-      }
-      else if (Array.isArray(filters[0])) {
-        if (filters[0].indexOf(element.TYPE) != -1) {
+      try {
+        if (filters.includes(element.ID) && kv["ID"]) {
           result.push(element);
         }
-      }
+        else if (filters.includes(element.TYPE) && kv["@basic_expression"]["lop"] == "TYPE" || filters.includes(element.TYPE) && kv["@basic_expression"]["rop"]["lop"] == "TYPE") {
+          result.push(element);
+        }
+        else if (Array.isArray(filters[0])) {
+          if (filters[0].indexOf(element.TYPE) != -1) {
+            result.push(element);
+          }
+        }
+      } catch { }
     });
     return result;
   }
